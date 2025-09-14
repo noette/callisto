36bdbb5bc6473e6f2e32c1d564a1a0273a361a95
noette <44951495+noette@users.noreply.github.com>
2025-09-14 15:21:26 -0400

    debugging 2

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,10 @@ const config = {
     adapter: adapter({
       fallback: "src/404.html",
       out: "build",
+      isr: {
+        expiration: 60 * 60,
+        allowQuery: ["code"]
+      }
     }),
     paths: {
       base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
