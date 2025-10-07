696c3fdbd744bbae8a43a9a36b7a76fdad4502bb
noette <fzheng413@gmail.com>
2025-10-07 00:08:28 -0400

    more missing files

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
       fallback: "src/404.html",
-      output: "build",
+      out: "build",
     }),
     paths: {
       base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
